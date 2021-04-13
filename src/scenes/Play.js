2fdc85962381fb1ecf00231df15cae2bd54f6885
Sky Peterson <katherine.ev.peterson@gmail.com>
2021-04-12 22:44:08 -0700

    Implemented game clock and replay function.

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -54,16 +54,50 @@ class Play extends Phaser.Scene{
             }),
             frameRate: 30
         });
+
+        // initialize score
+        this.p1Score = 0;
+        // display score
+        let scoreConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+              top: 5,
+              bottom: 5,
+            },
+            fixedWidth: 100
+        }
+        this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score, scoreConfig);
+          
+        // GAME OVER flag
+        this.gameOver = false;
+
+        // 60-second play clock
+        scoreConfig.fixedWidth = 0;
+        this.clock = this.time.delayedCall(60000, () => {
+            this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5);
+            this.gameOver = true;
+          }, null, this);
     }
 
     update() {
+        // check key input for restart
+        if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)){
+            this.scene.restart();
+        }
         this.starfield.tilePositionX -= starSpeed;
-        // update rocket
-        this.p1Rocket.update();
-        // update spaceships (x3)
-        this.ship01.update();
-        this.ship02.update();
-        this.ship03.update();
+        if(!this.gameOver){
+            // update rocket
+            this.p1Rocket.update();
+            // update spaceships (x3)
+            this.ship01.update();
+            this.ship02.update();
+            this.ship03.update();
+        }
 
         // check collisions
         if(this.checkCollision(this.p1Rocket, this.ship03)){
@@ -104,5 +138,8 @@ class Play extends Phaser.Scene{
             ship.alpha = 1;                 // make ship visible again
             boom.destroy();                 // remove explosion sprite
         });
+        // score add and repaint
+        this.p1Score += ship.points;
+        this.scoreLeft.text = this.p1Score;
     }
 }   
